63480346a158fe8e7c57132e3def39218517793d
Joel Rosen <joel@velarium.com>
2022-06-14 06:37:56 -0600

    methodcommit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+string userName = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+string age = Console.ReadLine();
+
+Console.WriteLine("What is your cat's name?");
+string catName = Console.ReadLine();
+
+Console.WriteLine("What color is your cat?");
+string catColor = Console.ReadLine();
+
+Console.WriteLine($" There was a man name {userName}, he was {age} years old.  He had a cat name {catName}.  His cat's color was  {catColor}");
+
